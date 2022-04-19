6bf3c3da1393552f786a03cc426ae58043989d2a
NadavOps <72969918+NadavOps@users.noreply.github.com>
2022-04-19 11:10:47 +0300

    xcode install alias

--- .zshrc.d/alias.mac.sh
+++ .zshrc.d/alias.mac.sh
@@ -2,3 +2,4 @@ autoload bashcompinit && bashcompinit
 autoload -Uz compinit && compinit
 
 alias mac_dns_cache_cleaner='dscacheutil -flushcache && sudo killall -HUP mDNSResponder'
+alias mac_xcode_install='xcode-select --install'
