ed2e7ecc28e024da742702eb7f9a9fc57defe704
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-01-22 14:05:37 +0200

    bash iterm bindkey comment

--- .bashrc.d/bindkey.iterm2.sh
+++ .bashrc.d/bindkey.iterm2.sh
@@ -1,5 +1,6 @@
 [[ $TERM_PROGRAM = "iTerm.app" ]] || return 0
 
+# bind key article https://www.computerhope.com/unix/bash/bind.htm
 # Allow using the option (alt) key to move through words, iterm does not allow that by default
 # Another way to to achieve it is by using the ".inputrc" file
 bind '"\e\e[D": backward-word'
