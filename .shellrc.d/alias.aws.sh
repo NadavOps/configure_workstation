c4fb2e4bba079f99258dcd9cba82ad0542255cd3
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-02-14 18:29:32 +0200

    public ECR

--- .shellrc.d/alias.aws.sh
+++ .shellrc.d/alias.aws.sh
@@ -13,3 +13,11 @@ aws_ecr_login() {
     fi
     aws ecr get-login-password --region "$aws_region" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com"
 }
+
+aws_ecr_login_public() {
+    local aws_region
+    aws_region="${AWS_REGION:-us-east-1}"
+    bash_logging DEBUG "\"AWS_REGION\" is set to the region: \"$aws_region\""
+    bash_logging INFO "Attempt to login into public.ecr.aws in \"$aws_region\""
+    aws ecr-public get-login-password --region "$aws_region" | docker login --username AWS --password-stdin public.ecr.aws
+}
