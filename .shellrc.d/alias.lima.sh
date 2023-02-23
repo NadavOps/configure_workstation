c681dc495df9f65858427ef5b1e4202c94f8ea19
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-02-23 19:30:34 +0200

    lima

---
+++ .shellrc.d/alias.lima.sh
@@ -0,0 +1,10 @@
+## Lima, unused, we using colima instead
+[[ $(command -v lima) ]] || return 0
+
+alias docker_context_current='docker context ls | grep "*"'
+docker_context_lima() {
+    local vm
+    [[ -z "$1" ]] && vm="lima" || vm="$1"
+    docker context inspect "$vm" &> /dev/null || docker context create "$vm" --docker "host=unix://$HOME/.$vm/docker/sock/docker.sock"
+    docker context use "$vm"
+}
