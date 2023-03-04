12c1f493691f6a422a0d67abb689caff3bfda7d6
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-03-04 17:04:27 +0200

    aws

--- .shellrc.d/alias.aws.sh
+++ .shellrc.d/alias.aws.sh
@@ -21,3 +21,33 @@ aws_ecr_login_public() {
     bash_logging INFO "Attempt to login into public.ecr.aws in \"$aws_region\""
     aws ecr-public get-login-password --region "$aws_region" | docker login --username AWS --password-stdin public.ecr.aws
 }
+
+aws_verify_profile() {
+    local profile_name
+    profile_name="${1:-default}"
+    bash_logging DEBUG "Looking for aws profile: \"$profile_name\""
+    aws configure list --profile "$profile_name" &> /dev/null && return 0
+    bash_logging ERROR "Profile \"$profile_name\" was not found. here are the avilable profiles"
+    aws configure list-profiles
+    return 1
+}
+
+aws_get_sso_credentials() {
+    local recent_token_file sso_token_dir access_token profile_name profile_account_id profile_sso_role profile_region credentials_properties
+    sso_token_dir="$HOME/.aws/sso/cache"
+    recent_token_file="$(ls -ltah "$sso_token_dir" | head -n 2 | tail -1 | awk '{print $9}')"
+    access_token="$(cat $sso_token_dir/$recent_token_file | jq -r .accessToken)"
+    profile_name="${1:-default}"
+    aws_verify_profile "$profile_name" || return 1
+    profile_account_id=$(aws configure get sso_account_id --profile "$profile_name")
+    profile_sso_role=$(aws configure get sso_role_name --profile "$profile_name")
+    profile_region=$(aws configure get sso_region --profile "$profile_name")
+    credentials_properties=$(aws sso get-role-credentials --account-id "$profile_account_id" \
+        --role-name "$profile_sso_role" \
+        --access-token "$access_token" \
+        --region "$profile_region")
+    export AWS_ACCESS_KEY_ID=$(echo "$credentials_properties" | jq -r ".roleCredentials.accessKeyId")
+    export AWS_SECRET_ACCESS_KEY=$(echo "$credentials_properties" | jq -r ".roleCredentials.secretAccessKey")
+    export AWS_SESSION_TOKEN=$(echo "$credentials_properties" | jq -r ".roleCredentials.sessionToken")
+    aws sts get-caller-identity
+}
