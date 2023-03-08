24197a5be541622864ec7a7abca8c9ca6f46a7a6
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-03-08 11:15:29 +0200

    github curl

--- .shellrc.d/alias.github_curl.sh
+++ .shellrc.d/alias.github_curl.sh
@@ -10,14 +10,14 @@ github_dispatch_workflow() {
     if [[ -z "$git_repository" || -z "$workflow_filename" || -z "$branch_name" || -z "$inputs" ]]; then
         bash_logging ERROR "not all parameters supplied"
     fi
-    git_token="$(cat ${5-$GIT_TOKEN})"
+    git_token="${5-$GIT_TOKEN}"
     bash_logging DEBUG "token: $git_token"
     git_owner="${6-$GIT_OWNER}"
     bash_logging DEBUG "owner: $git_owner"
     if [[ ! -f "$git_token" ]]; then bash_logging ERROR "Git token: \"$git_token\" was not found"; return 1; fi
     curl -L -X POST \
         -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer $git_token"\
+        -H "Authorization: Bearer $(cat $git_token)"\
         -H "X-GitHub-Api-Version: 2022-11-28" \
         https://api.github.com/repos/$git_owner/$git_repository/actions/workflows/$workflow_filename/dispatches \
         -d '{"ref":"'$branch_name'","inputs":{'$inputs'}}'
