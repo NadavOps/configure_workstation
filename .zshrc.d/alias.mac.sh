64fce57946e7de9b825816b2416c07d0c4febb4c
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-05-11 18:00:57 +0300

    mac show routing tables

--- .zshrc.d/alias.mac.sh
+++ .zshrc.d/alias.mac.sh
@@ -3,3 +3,4 @@ autoload -Uz compinit && compinit
 
 alias mac_dns_cache_cleaner='dscacheutil -flushcache && sudo killall -HUP mDNSResponder'
 alias mac_xcode_install='xcode-select --install'
+alias mac_route_show_ipv4='netstat -rn -f inet'
