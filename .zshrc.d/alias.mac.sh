9870b213892268d3c412fa2b40280f982fa8eaf2
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-08-31 13:45:20 +0300

    mac alias

--- .zshrc.d/alias.mac.sh
+++ .zshrc.d/alias.mac.sh
@@ -3,4 +3,5 @@ autoload -Uz compinit && compinit
 
 alias mac_dns_cache_cleaner='dscacheutil -flushcache && sudo killall -HUP mDNSResponder'
 alias mac_xcode_install='xcode-select --install'
-alias mac_route_show_ipv4='netstat -rn -f inet'
+alias mac_show_route_ipv4='netstat -rn -f inet'
+alias mac_show_ports='netstat -anvp tcp | awk "NR<3 || /LISTEN/"'
