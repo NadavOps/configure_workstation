f4067887e05df9c3764ed9c600396a5beaf1491d
NadavOps <72969918+NadavOps@users.noreply.github.com>
2023-11-01 02:04:53 +0200

    git

--- .shellrc.d/alias.git.sh
+++ .shellrc.d/alias.git.sh
@@ -1,58 +1,86 @@
 ## Git
 [[ $(command -v git) ]] || return 0
 
-## Git aliases
-git_commit_all()
-{
-    git_commit_help()
-    {
-        bash_logging DEBUG "git_commit_all [-h](help) [-f](force) [-m cusomt_commit_message] [-p](pull first)" 1>&2
-        return
+git_commit_all() (
+    git_commit_all_help() {
+        bash_logging DEBUG "git_commit_all \\
+            [-p](pull_first) \\
+            [-m][commit message] \\
+            [-c][commit hash] \\
+            [-s](push_sensitive_branch) \\
+            [-h](help)" 1>&2
     }
-    trap "unset -f git_commit_help" EXIT
-    local OPTIND o h f m p status_code
-    while getopts "hfm:p" o; do
-        case "${o}" in
-            h)
-                git_commit_help; return
-                ;;
-            f)
-                f="--force"
-                ;;
-            m)
-                m="${OPTARG}"
-                ;;
-            p)
-                p="--pull_first"
-                ;;
-            *)
-                git_commit_help; return
-                ;;
+
+    git_commit_all_run_command() {
+        local command
+        command="$1"
+        bash_logging DEBUG "Running the command \"$command\""
+        if ! eval "$command"; then bash_logging ERROR "The command \"$command\" failed (status code: $?)"; return 1; fi
+    }
+
+    git_commit_all_commit_message() {
+        local commit_message commit_hash
+        commit_message="$1"
+        commit_hash="$2"
+        if [[ -n "$commit_message" ]]; then
+            git_commit_all_run_command "git commit -m \"$commit_message\"" || return 1
+        else
+            if [[ -z "$commit_hash" ]]; then
+                commit_hash=$(git rev-parse HEAD)
+            fi
+            git_commit_all_run_command "git commit --fixup $commit_hash" || return 1
+            # git_commit_all_run_command "git commit -m \"fixup! $commit_hash\"" || return 1
+        fi
+    }
+
+    local o h p m c s
+    while getopts "hpm:c:s" o; do
+        case "$o" in
+            h) git_commit_all_help; return 0;;
+            p) p="--pull_first";;
+            m) m="${OPTARG}";;
+            c) c="${OPTARG}";;
+            s) s="--push_sensitive_branch";;
+            *) git_commit_all_help; return 1;;
         esac
     done
-    shift $((OPTIND-1))
 
-    local branch_name default_remote_branch_name
-    if ! git rev-parse --git-dir &> /dev/null; then bash_logging ERROR "$(pwd) is not a git directory" && return 1; fi
-    branch_name=$(git rev-parse --abbrev-ref HEAD)
-    default_remote_branch_name=$(git rev-parse --abbrev-ref refs/remotes/origin/HEAD 2> /dev/null | rev | cut -d "/" -f1 | rev)
-    if [[ "$branch_name" == "main" || $branch_name == "master" || "$branch_name" == "$default_remote_branch_name" ]] && [[ $f != "--force" ]]; then
-        bash_logging ERROR "Branch \"$branch_name\" is sensitive. add -f to push anyway"
+    if [[ -n "$m" && -n "$c" ]]; then
+        git_commit_all_help
+        bash_logging ERROR "Pick either to commit with message or to fixup a specific commit, dont provide both -m and -c"
         return 1
     fi
 
-    if [[ "$p" == "--pull_first" ]]; then git pull; fi
-    if [[ "$?" -ne 0 ]]; then bash_logging ERROR "git pull failed" && return; fi
-    if [[ -z "$m" ]]; then m="squash this commit later"; fi
+    local branch_name default_remote_branch_name status_code
+    if ! git rev-parse --git-dir &> /dev/null; then bash_logging ERROR "$(pwd) is not a git directory" && return 1; fi
+    branch_name=$(git rev-parse --abbrev-ref HEAD)
+    default_remote_branch_name=$(git rev-parse --abbrev-ref refs/remotes/origin/HEAD 2> /dev/null | rev | cut -d "/" -f1 | rev)
 
-    if [[ $branch_name == "main" || $branch_name == "master" ]] && [[ $f == "--force" ]]; then
-        bash_logging WARN "Pushing with "$f" to \"$branch_name\" branch"
+    if [[ "$p" == "--pull_first" ]]; then git_commit_all_run_command "git pull" || return 1; fi
+
+    if [[ -z "$(git status --porcelain)" ]]; then
+        bash_logging INFO "Working tree is clean, nothing to add or commit"
+    elif [[ -n "$(git diff --cached --exit-code)" ]]; then
+        git_commit_all_commit_message "$m" "$c" || return 1
+    # elif [[ -n "$(git diff --exit-code)" ]]; then
+    else
+        git_commit_all_run_command "git add -A" || return 1
+        git_commit_all_commit_message "$m" "$c" || return 1
     fi
-    bash_logging DEBUG "Running the command:
-                               git add -A && git commit -m \"$m\" && git push"
-    git add -A && git commit -m "$m"
-    if [[ "$?" -ne 0 ]]; then bash_logging WARN "staging or commiting to local failed" && return; fi
+
+    if [[ "$s" != "--push_sensitive_branch" ]]; then
+        if [[ "$branch_name" == "main" || "$branch_name" == "master" || "$branch_name" == "$default_remote_branch_name" ]]; then
+            git_commit_all_help
+            bash_logging ERROR "Branch \"$branch_name\" is sensitive."
+            return 1
+        fi
+    else
+        if [[ "$branch_name" == "main" || "$branch_name" == "master" || "$branch_name" == "$default_remote_branch_name" ]]; then
+            bash_logging WARN "Branch \"$branch_name\" is sensitive. Pushing as \"$s\" was supplied"
+        fi
+    fi
     git push
+
     status_code=$?
     if [[ "$status_code" -eq 128 ]]; then
         bash_logging WARN "\"branch_name\" might not exist, should we run:
@@ -69,4 +97,28 @@ git_commit_all()
         git reset HEAD^
         bash_logging INFO "The function support pulling first with \"-p\""
     fi
+)
+
+git_rebase_autosquash() {
+    local commit_hash branch_name default_remote_branch_name branching_point_commit
+    commit_hash="$1"
+    branch_name=$(git rev-parse --abbrev-ref HEAD)
+    default_remote_branch_name=$(git rev-parse --abbrev-ref refs/remotes/origin/HEAD 2> /dev/null | rev | cut -d "/" -f1 | rev)
+    if [[ -n "$commit_hash" ]]; then
+        git rebase -i --autosquash "$commit_hash"
+    elif [[ "$branch_name" == "$default_remote_branch_name" ]]; then
+        bash_logging ERROR "Rebasing in \"$default_remote_branch_name\" branch requires explicit commit hash."
+        git_log
+        return 1
+    else
+        branching_point_commit=$(git merge-base "$branch_name" "$default_remote_branch_name")
+        bash_logging DEBUG "Rebase on $branching_point_commit"
+        git rebase -i --autosquash $branching_point_commit
+    fi
+}
+
+git_log() {
+    local number_of_commits
+    number_of_commits="$1"
+    git log --pretty=format:"%C(auto)%h %ad %an %d %s" --date short --graph -n ${1-10}
 }
