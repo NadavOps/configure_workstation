f3b7b6ee2e7abfc881393f1d9e77366db59dd7b8
NadavOps <72969918+NadavOps@users.noreply.github.com>
2024-04-18 16:25:42 +0300

    nvm

--- .shellrc.d/alias.nvm.sh
+++ .shellrc.d/alias.nvm.sh
@@ -1,2 +1,8 @@
+export NVM_DIR="$HOME/.nvm"
+# Intel based
 [[ -s "/usr/local/opt/nvm/nvm.sh" ]] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ]] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# M chip based
+[[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
