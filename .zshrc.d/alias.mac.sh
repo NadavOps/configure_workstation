8106f60f75b48636184cb1ad115e9c1d14a7215b
NadavOps <72969918+NadavOps@users.noreply.github.com>
2025-01-08 12:07:48 +0200

    mac caffeinate

--- .zshrc.d/alias.mac.sh
+++ .zshrc.d/alias.mac.sh
@@ -5,3 +5,10 @@ alias mac_dns_cache_cleaner='dscacheutil -flushcache && sudo killall -HUP mDNSRe
 alias mac_xcode_install='xcode-select --install'
 alias mac_show_route_ipv4='netstat -rn -f inet'
 alias mac_show_ports='netstat -anvp tcp | awk "NR<3 || /LISTEN/"'
+
+mac_caffeinate() {
+    local timeout=${1:-3600}
+    if ! command -v caffeinate > /dev/null; then echo "ERROR: caffeinate utility is not found on the system" && return 1; fi
+    echo "INFO: mac caffeinate for $timeout seconds"
+    caffeinate -d -i -m -t $timeout
+}
