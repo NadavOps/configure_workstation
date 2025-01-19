2323a7dcaac5804e1d5aa12ccddc130b650ad1a2
NadavOps <72969918+NadavOps@users.noreply.github.com>
2025-01-19 12:43:34 +0200

    mac caffeinate

--- .zshrc.d/alias.mac.sh
+++ .zshrc.d/alias.mac.sh
@@ -8,7 +8,9 @@ alias mac_show_ports='netstat -anvp tcp | awk "NR<3 || /LISTEN/"'
 
 mac_caffeinate() {
     local timeout=${1:-3600}
+	local hours=$(($timeout / 3600))
+	local minutes=$((($timeout % 3600) / 60))
     if ! command -v caffeinate > /dev/null; then echo "ERROR: caffeinate utility is not found on the system" && return 1; fi
-    echo "INFO: mac caffeinate for $timeout seconds"
+    echo "INFO: mac caffeinate for $timeout seconds ($hours hours and $minutes minutes)"
     caffeinate -d -i -m -t $timeout
 }
