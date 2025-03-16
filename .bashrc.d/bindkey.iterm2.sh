1c92c1cc0cd2006d5cbd15bb45b650ad3bb19cf8
NadavOps <72969918+NadavOps@users.noreply.github.com>
2025-03-16 11:36:10 +0200

    Bindkeys

--- .bashrc.d/bindkey.iterm2.sh
+++ .bashrc.d/bindkey.iterm2.sh
@@ -3,5 +3,5 @@
 # bind key article https://www.computerhope.com/unix/bash/bind.htm
 # Allow using the option (alt) key to move through words, iterm does not allow that by default
 # Another way to to achieve it is by using the ".inputrc" file
-bind '"\e\e[D": backward-word'
-bind '"\e\e[C": forward-word'
+bind '"\e[1;3C": forward-word'
+bind '"\e[1;3D": backward-word'
--- .zshrc.d/bindkey.iterm2.sh
+++ .zshrc.d/bindkey.iterm2.sh
@@ -1,6 +1,8 @@
 [[ $TERM_PROGRAM = "iTerm.app" ]] || return 0
 
-# Taken from here: https://apple.stackexchange.com/questions/154292/iterm-going-one-word-backwards-and-forwards
+# Bind key article https://apple.stackexchange.com/questions/154292/iterm-going-one-word-backwards-and-forwards
 # Allow using the option (alt) key to move through words, iterm does not allow that by default
-bindkey "[D" backward-word
-bindkey "[C" forward-word
+# bindkey alone will output the bindings
+# '⌘ + ,' -> 'Preferences' -> 'Profiles' -> 'Keys' -> change left+right option key to 'Esc+'
+bindkey "\e[1;3C" forward-word   # Alt + →
+bindkey "\e[1;3D" backward-word  # Alt + ←
